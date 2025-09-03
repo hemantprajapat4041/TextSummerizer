beeb9fd6b77ae2e2f86cb8f355ac8a03357f26dd
hemantprajapat4041 <hemantprajapat72400@gmail.com>
2025-09-03 17:50:05 +0530

    model training completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.TextSummerizer.pipeline.stage_2_data_transformation_pipeline import DataTransformationTrainingPipeline
 from src.TextSummerizer.logging import logger
 from src.TextSummerizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.TextSummerizer.pipeline.stage_3_model_trainer_pipeline import ModelTrainerTrainingPipeline
 
 STAGE_NAME="Data Ingestion Stage"
 
@@ -21,6 +22,18 @@ try:
     logger.info(f"stage{STAGE_NAME} INITIATED")
     data_transformation_pipeline=DataTransformationTrainingPipeline()
     data_transformation_pipeline.initiate_data_transformation()
+    logger.info(f"stage{STAGE_NAME} COMPLETED")
+
+except Exception as e:
+    logger.exception(e)
+    raise(e)
+
+STAGE_NAME="Model Training Stage"
+
+try:
+    logger.info(f"stage{STAGE_NAME} INITIATED")
+    model_trainer_pipeline=ModelTrainerTrainingPipeline()
+    model_trainer_pipeline.initiate_model_training()
     logger.info(f"stage{STAGE_NAME} COMPLETED")
 
 except Exception as e:
